9e4a6c53fbe156693f0efddb0018f33862f3218d
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-09-14 22:20:49 -0700

    cpu.py updated

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,9 +5,20 @@ import sys
 class CPU:
     """Main CPU class."""
 
+
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256 # memory
+        self.register = [0] * 8 # registers
+        self.pc = 0 # Program Counter 
+        self.running = False
+
+    def ram_read( self, MAR):
+        """should accept the address to read and return the value stored there."""
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -60,6 +71,43 @@ class CPU:
 
         print()
 
+
+
+
+    def PRN(self):
+        """Print Register: print the numeric value stored in a given register"""
+        address = self.ram_read(self.pc + 1)
+        print(self.reg[address])
+
+    def HLT(self):
+        """Halt: halt the CPU (& exit the emulator)"""
+        self.running = False 
+
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True 
+
+    # Machine Code value binary 
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        while self.running:
+
+    # initialize Ir with value of ram at index pc 
+            ir = self.ram_read(self.pc)
+
+            if ir == LDI:  # PRINT_BEEJ
+                self.LDI()
+                pc += 1
+
+            elif ir == PRN:
+                self.PRN() 
+                pc += 2 
+
+            elif ir == HLT:  # SAVE_REG
+                self.HLT() 
+                pc += 3 
+
+            else:
+                print(f"Unknown instruction {b}")
